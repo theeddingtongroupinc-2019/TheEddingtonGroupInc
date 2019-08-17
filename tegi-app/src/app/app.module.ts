eba38e36ac26f07c01eb931b10b6f5114a56143c
agtrevino <39744373+agtrevino@users.noreply.github.com>
2019-08-17 18:57:41 -0500

    updated css

--- tegi-app/src/app/app.module.ts
+++ tegi-app/src/app/app.module.ts
@@ -4,30 +4,41 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRippleModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TegiHomeComponent } from './tegi-home/tegi-home.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactDialogComponent } from './contact-dialog/contact-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TegiHomeComponent,
     MainNavComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ContactDialogComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatDialogModule,
     MatToolbarModule,
+    MatRippleModule,
     MatButtonModule,
+    MatSidenavModule,
     MatExpansionModule,
     AppRoutingModule
   ],
+  entryComponents: [
+    ContactDialogComponent
+  ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tegi-app/src/app/tegi-home/tegi-home.component.ts
+++ tegi-app/src/app/tegi-home/tegi-home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactDialogComponent } from '../contact-dialog/contact-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-tegi-home',
   templateUrl: './tegi-home.component.html',
   styleUrls: ['./tegi-home.component.css']
 })
-export class TegiHomeComponent implements OnInit {
 
-  constructor() { }
+export class TegiHomeComponent implements OnInit {
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ContactDialogComponent, {
+     
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
